3837d1c73ad1caffef915d81edc4735fa8e0ff47
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-03-25 12:35:30 +0100

    starting event form componetizing

--- S-8/front_end/src/features/events/types/mapContextTypes.ts
+++ S-8/front_end/src/features/events/types/mapContextTypes.ts
@@ -11,15 +11,15 @@ export interface MapEvent {
   lng: number;
 }
 
-// export interface MapContextData {
-//   mapsData: MapEvent[];
-//   loading: boolean;
-//   error: string | null;
-//   fetchMapEvents: () => Promise<void>;
-//   createMapEvent: (eventData: Omit<MapEvent, "_id">) => Promise<void>;
-//   deleteMapEvent: (id: string, title: string) => Promise<void>;
-//   updateMapEvent: (id: string, eventData: MapEvent) => Promise<void>;
-// }
+export interface MapContextData {
+  mapsData: MapEvent[];
+  loading: boolean;
+  error: string | null;
+  fetchMapEvents: () => Promise<void>;
+  createMapEvent: (eventData: Omit<MapEvent, "_id">) => Promise<void>;
+  deleteMapEvent: (id: string, title: string) => Promise<void>;
+  updateMapEvent: (id: string, eventData: MapEvent) => Promise<void>;
+}
 
 export interface MapContextType {
   mapsData: CalendarEvent[];
